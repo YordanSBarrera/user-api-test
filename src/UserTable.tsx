68bc9287463c0e1214983f6c0617c53393c383fa
Yordan Saldaña <yordansaldanabarrera@gmail.com>
2025-02-03 17:01:23 -0300

    add table color

--- src/UserTable.tsx
+++ src/UserTable.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { User } from "./util";
 
 type UserTableProps = {
@@ -5,33 +6,54 @@ type UserTableProps = {
 };
 
 const UserTable = ({ users }: UserTableProps) => {
+  const [hasColor, setHasColor] = useState<boolean>(false);
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <td>Foto</td>
-          <td>Nombre</td>
-          <td>Apellido</td>
-          <td>Pais</td>
-          <td>acciones</td>
-        </tr>
-      </thead>
-      <tbody>
-        {users.map((user) => {
-          return (
-            <tr>
-              <td>
-                <img src={user.picture.medium} alt="" />
-              </td>
-              <td>{user.name.first}</td>
-              <td>{user.name.last}</td>
-              <td>{user.location.country}</td>
-              <td>acciones</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <>
+      <div style={{ marginBottom: "10px" }}>
+        <button
+          onClick={() => {
+            setHasColor(!hasColor);
+          }}
+        >
+          {hasColor ? "Dar Color Tabla" : "Quitar Color Tabla"}
+        </button>
+      </div>
+
+      <table className="table">
+        <thead>
+          <tr>
+            <td>Foto</td>
+            <td>Nombre</td>
+            <td>Apellido</td>
+            <td>Pais</td>
+            <td>acciones</td>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => {
+            return (
+              <tr
+                style={{
+                  backgroundColor: hasColor
+                    ? index % 2
+                      ? "#97b6d6"
+                      : "#7bba9b"
+                    : "",
+                }}
+              >
+                <td>
+                  <img src={user.picture.medium} alt="" />
+                </td>
+                <td>{user.name.first}</td>
+                <td>{user.name.last}</td>
+                <td>{user.location.country}</td>
+                <td>acciones</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
   );
 };
 export default UserTable;
