750117bbe69f326dbbba7b2a5882a685863f5387
Yordan Saldaña <yordansaldanabarrera@gmail.com>
2025-02-03 14:26:05 -0300

    Create util.ts

---
+++ src/util.ts
@@ -0,0 +1,59 @@
+export type User = {
+  gender: string;
+  name: Name;
+  location: Location;
+  email: string;
+  login: {
+    uuid: string;
+    username: string;
+    password: string;
+    salt: string;
+    md5: string;
+    sha1: string;
+    sha256: string;
+  };
+  dob: {
+    date: string;
+    age: number;
+  };
+  registered: {
+    date: string;
+    age: number;
+  };
+  phone: string;
+  cell: string;
+  id: {
+    name: string;
+    value: string;
+  };
+  picture: {
+    large: string;
+    medium: string;
+    thumbnail: string;
+  };
+  nat: string;
+};
+
+type Name = {
+  title: string;
+  first: string;
+  last: string;
+};
+type Location = {
+  street: {
+    number: number;
+    name: string;
+  };
+  city: string;
+  state: string;
+  country: string;
+  postcode: number;
+  coordinates: {
+    latitude: string;
+    longitude: string;
+  };
+  timezone: {
+    offset: string;
+    description: string;
+  };
+};
